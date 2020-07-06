4d320a0023ce82d8b250b8185b66a5a1730adc67
Frain <bekrenyov.b@gmail.com>
2020-07-06 20:45:11 +0300

    Added DB

---
+++ MemoryCards/CardModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryCards
+{
+    public class CardModel
+    {
+        public int ID { get; set; }
+        public string FirstPage { get; set; }
+        public string SecondPage { get; set; }
+        public string Due { get; set; }
+        public int Group { get; set; }
+
+        public CardModel()
+        {
+
+        }
+
+        public CardModel(int ID, string FirstPage, string SecondPage, string Due, int Group)
+        {
+            this.ID = ID;
+            this.FirstPage = FirstPage;
+            this.SecondPage = SecondPage;
+            this.Due = Due;
+            this.Group = Group;
+        }
+    }
+}
---
+++ MemoryCards/DBAccess.cs
@@ -0,0 +1,52 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+
+namespace MemoryCards
+{
+    public class SqliteDataAccess
+    {
+
+        public static List<CardModel> LoadCards()
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                List<CardModel> cards = cnn.Query<CardModel>("select * from Cards").ToList();
+                return cards;
+            }
+        }
+
+        public static CardModel LoadACard(CardModel card)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                return cnn.Query<CardModel>("select * from Tasks WHERE ID = @ID", card).First();
+            }
+        }
+
+        public static void SaveCard(CardModel card)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                cnn.Execute("insert into Cards (ID, FirstPage, SecondPage, Due, Group) values (@ID, @FirstPage, @SecondPage, @Due, @Group);", card);
+            }
+        }
+
+        public static void DeleteCard(CardModel card)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                cnn.Execute("Delete From Cards WHERE ID = @ID", card);
+            }
+        }
+
+        private static string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
+
